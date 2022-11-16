44096656e20dbcfa6af378bdd01019bdc7f614f6
Maciej Rymarz <maciej.rymarz@escolasoft.com>
2022-11-16 14:47:55 +0100

    WIP

--- tests/Command/MergeTranslationsOfPermissionsCommandTest.php
+++ tests/Command/MergeTranslationsOfPermissionsCommandTest.php
@@ -3,7 +3,6 @@
 namespace EscolaLms\Translations\Tests\Command;
 
 use EscolaLms\Translations\Console\Command\MergeTranslationsOfPermissionsCommand;
-use EscolaLms\Translations\Database\Seeders\TranslationsPermissionSeeder;
 use EscolaLms\Translations\Enum\TranslationsPermissionsEnum;
 use EscolaLms\Translations\Tests\TestCase;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
@@ -12,12 +11,6 @@ use Illuminate\Support\Facades\Config;
 class MergeTranslationsOfPermissionsCommandTest extends TestCase
 {
     use DatabaseTransactions;
-
-    protected function setUp(): void
-    {
-        parent::setUp();
-        $this->seed(TranslationsPermissionSeeder::class);
-    }
 
     public function testCreateFileWithTranslations(): void
     {
